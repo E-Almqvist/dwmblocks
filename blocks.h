c83ba2a8eecbbe02c0eb04be108309448fe635bb
E. Almqvist <elalmqvist@gmail.com>
2021-10-19 21:49:32 +0200

    Blocks

---
+++ blocks.h
@@ -0,0 +1,12 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "cmus.sh", 1, 0},
+	{"", "netinfo.sh", 4, 0},
+	{"Vol: ", "pavolume.sh", 1, 0},
+	{"", "printf \"{$(date +\"%H:%M:%S, %A, %Y-%m-%d\")}\"", 1, 0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = "    ";
+static unsigned int delimLen = 4;
