b26416783c9a435c033a6298713adc2c111b8e7f
E. Almqvist <elalmqvist@gmail.com>
2021-10-19 22:11:39 +0200

    Config

--- blocks.h
+++ blocks.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cmus.sh", 1, 0},
 	{"", "netinfo.sh", 4, 0},
-	{"Vol: ", "pavolume.sh", 1, 0},
+	{"Vol: ", "pavolume.sh", 2, 0},
 	{"", "printf \"{$(date +\"%H:%M:%S, %A, %Y-%m-%d\")}\"", 1, 0},
 };
 
