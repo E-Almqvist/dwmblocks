c1624b3a9f86264c20e71eddd4f3ddd22afc88b9
E. Almqvist <elalmqvist@gmail.com>
2021-10-20 21:42:53 +0200

    Reminders

--- blocks.h
+++ blocks.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cmus.sh", 1, 0},
-	{"", "netinfo.sh", 4, 0},
-	{"Vol: ", "pavolume.sh", 2, 0},
+	{"", "scripts/cmus.sh", 0, 3}, 	// TODO: fix actual path
+	{"", "scripts/netinfo.sh", 4, 2}, // TODO: signals
+	{"Vol: ", "scripts/pavolume.sh", 0, 1}, // TODO: make this work
 	{"", "printf \"{$(date +\"%H:%M:%S, %A, %Y-%m-%d\")}\"", 1, 0},
 };
 
