b260fe87e6570703e5aba7257b263a6484bd545f
E. Almqvist <elalmqvist@gmail.com>
2021-10-21 15:19:30 +0200

    Minor fix

--- blocks.h
+++ blocks.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "scripts/cmus.sh", 0, 3}, 	// TODO: fix actual path
-	{"", "scripts/netinfo.sh", 4, 2}, // TODO: signals
-	{"Vol: ", "scripts/pavolume.sh", 0, 1}, // TODO: make this work
+	{"", "cmus.sh", 1, 3}, 	// TODO: fix actual path
+	{"", "netinfo.sh", 4, 2}, // TODO: signals
+	{"Vol: ", "pavolume.sh", 0, 1}, // TODO: make this work
 	{"", "printf \"{$(date +\"%H:%M:%S, %A, %Y-%m-%d\")}\"", 1, 0},
 };
 
