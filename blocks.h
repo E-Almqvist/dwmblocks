220b261a4d4603ec78e22efccb3554f9fc6368cf
E. Almqvist <elalmqvist@gmail.com>
2021-10-21 15:47:16 +0200

    Fixed filepath

--- blocks.h
+++ blocks.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cmus.sh", 1, 3}, 	// TODO: fix actual path
-	{"", "netinfo.sh", 4, 2}, // TODO: signals
-	{"Vol: ", "pavolume.sh", 0, 1}, // TODO: make this work
+	{"", "/opt/scripts/cmus.sh", 1, 3}, 	// TODO: fix actual path
+	{"", "/opt/scripts/netinfo.sh", 4, 2}, // TODO: signals
+	{"Vol: ", "/opt/scripts/pavolume.sh", 0, 1}, // TODO: make this work
 	{"", "printf \"{$(date +\"%H:%M:%S, %A, %Y-%m-%d\")}\"", 1, 0},
 };
 
