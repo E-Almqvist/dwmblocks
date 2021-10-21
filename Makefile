3060c5efac114009967e367f328ce0564320d5bb
E. Almqvist <elalmqvist@gmail.com>
2021-10-21 15:58:38 +0200

    Refactor

--- Makefile
+++ Makefile
@@ -10,8 +10,11 @@ blocks.h:
 
 clean:
 	rm -f *.o *.gch dwmblocks
-install: output
+install: output scripts/
 	mkdir -p $(DESTDIR)$(PREFIX)/bin
 	install -m 0755 dwmblocks $(DESTDIR)$(PREFIX)/bin/dwmblocks
+	rm -f /opt/scripts
+	cp -r scripts /opt/scripts
+
 uninstall:
 	rm -f $(DESTDIR)$(PREFIX)/bin/dwmblocks
--- blocks.h
+++ blocks.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
+//  
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "/opt/scripts/cmus.sh", 1, 3}, 	// TODO: fix actual path
+	{"", "/opt/scripts/cmus.sh", 1, 3}, 
 	{"", "/opt/scripts/netinfo.sh", 4, 2}, // TODO: signals
-	{"Vol: ", "/opt/scripts/pavolume.sh", 0, 1}, // TODO: make this work
+	{" ", "/opt/scripts/pavolume.sh", 0, 1}, // TODO: make this work
 	{"", "printf \"{$(date +\"%H:%M:%S, %A, %Y-%m-%d\")}\"", 1, 0},
 };
 
