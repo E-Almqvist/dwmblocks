5953d013e97a2bdddcb53f5b6db84094e197ba5d
E. Almqvist <elalmqvist@gmail.com>
2021-10-22 08:17:54 +0200

    Revert "Blocks battery"
    This reverts commit 44ba42c191ae60b3a2831f11ab57acf46b7eff25.

--- blocks.h
+++ blocks.h
@@ -5,7 +5,6 @@ static const Block blocks[] = {
 	{"", "/opt/scripts/cmus.sh", 1, 3}, 
 	{"", "/opt/scripts/netinfo.sh", 1, 2}, // TODO: signals
 	{"vol: ", "/opt/scripts/pavolume.sh", 0, 1}, // TODO: make this work
-	{"bat: ", "/opt/scripts/battery.sh", 1, 1}, // TODO: make this work
 	{"", "printf \"{$(date +\"%H:%M:%S, %A, %Y-%m-%d\")}\"", 1, 0},
 };
 
